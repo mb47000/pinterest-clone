5007bb5ff4f112da1046512f32e270b3e6a5c31f
Brousse.M <mbweb47@gmail.com>
2020-08-15 01:53:03 +0200

    debug-bundle + update PinsController.php

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
@@ -11,8 +14,17 @@ class PinsController extends AbstractController
     /**
      * @Route("/", name="pins")
      */
-    public function index()
+    public function index(EntityManagerInterface $em): Response
     {
+        $pin = new Pin;
+        $pin->setTitle('Title 4');
+        $pin->setDescription('Description 4');
+
+        $em->persist($pin);
+        $em->flush();
+
+        // dd($pin); pareil que dump($pin) suivi de die;
+        
         return $this->render('pins/index.html.twig');
     }
 
