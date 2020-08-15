0dba2165277e5184e1eb721ebd2ae19d35230204
Brousse.M <mbweb47@gmail.com>
2020-08-15 19:01:32 +0200

    csrf sec + pins form

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -3,29 +3,44 @@
 namespace App\Controller;
 
 use App\Entity\Pin;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PinRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PinsController extends AbstractController
 {
 
     /**
-     * @Route("/", name="pins")
+     * @Route("/", name="app_home")
      */
-    public function index(EntityManagerInterface $em): Response
-    {
-        $pin = new Pin;
-        $pin->setTitle('Title 4');
-        $pin->setDescription('Description 4');
-
-        $em->persist($pin);
-        $em->flush();
-
-        // dd($pin); pareil que dump($pin) suivi de die;
-        
-        return $this->render('pins/index.html.twig');
+    public function index(PinRepository $repo): Response
+    {   
+        return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
     }
 
+
+    /**
+     * @Route("/pins/create", name="app_pins_create", methods="GET|POST"))
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {   
+        if($request->isMethod('POST')){
+            $data = $request->request->all();
+
+            if ($this->isCsrfTokenValid('pins_create', $data['_token'])) {
+                $pins = new Pin;
+                $pins->setTitle($data['title']);
+                $pins->setDescription($data['description']);
+                $em->persist($pins);
+                $em->flush();
+            }
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('pins/create.html.twig');
+    }
 }
